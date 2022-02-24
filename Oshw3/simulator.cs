4160cd921ee506e5ec097e16e3567c9f67e7bdec
Joshua Prince <jdp098@shsu.edu>
2022-02-23 21:21:48 -0600

    working logic

--- Oshw3/simulator.cs
+++ Oshw3/simulator.cs
@@ -10,6 +10,57 @@ namespace Oshw3
 {
     class simulator
     {
+        public static void Turtle()
+        {
+            string url = "https://youtu.be/jQK6iUj9Uts?t=20";
+            try
+            {
+                System.Diagnostics.Process.Start(url);
+            }
+            catch
+            {
+                // hack because of this: https://github.com/dotnet/corefx/issues/10361
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    url = url.Replace("&", "^&");
+                    System.Diagnostics.Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    System.Diagnostics.Process.Start("xdg-open", url);
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    System.Diagnostics.Process.Start("open", url);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+        //used to create the new process name
+        public static string generatestring()
+        {
+            int length = 2;
+
+            // creating a StringBuilder object()
+            StringBuilder str_build = new StringBuilder();
+            Random random = new Random();
+
+            char letter;
+
+            for (int i = 0; i < length; i++)
+            {
+                double flt = random.NextDouble();
+                int shift = Convert.ToInt32(Math.Floor(25 * flt));
+                letter = Convert.ToChar(shift + 65);
+                str_build.Append(letter);
+            }
+            return str_build.ToString();
+            System.Console.WriteLine(str_build.ToString());
+        }
+
         public class Process
         {
             public int arrival { get; set; }
@@ -19,17 +70,9 @@ namespace Oshw3
             public int runtime { get; set; } = 0;
             public bool activated { get; set; } = false;
             public int endtime { get; set; }
+            public int tempcount { get; set; } = 0;
 
         }
-
-
-
-        public static int numprocesses = 4;
-        public static int time = 0;
-        public static int maxtime = 0;
-        public static int quantum = 2;
-        public static int count = 0;
-
         //loop to read input file and assgn each item to the list
         static List<Process> processes = new List<Process>() {
             new Process
@@ -69,61 +112,16 @@ namespace Oshw3
 
         };
 
+
+
         private static Process temp = new Process();
-
-        public static void Turtle()
-        {
-            string url = "https://youtu.be/jQK6iUj9Uts?t=20";
-            try
-            {
-                System.Diagnostics.Process.Start(url);
-            }
-            catch
-            {
-                // hack because of this: https://github.com/dotnet/corefx/issues/10361
-                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                {
-                    url = url.Replace("&", "^&");
-                    System.Diagnostics.Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
-                }
-                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-                {
-                    System.Diagnostics.Process.Start("xdg-open", url);
-                }
-                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-                {
-                    System.Diagnostics.Process.Start("open", url);
-                }
-                else
-                {
-                    throw;
-                }
-            }
-        }
-
-
-        //used to create the new process name
-        public static string generatestring()
-        {
-            int length = 2;
-
-            // creating a StringBuilder object()
-            StringBuilder str_build = new StringBuilder();
-            Random random = new Random();
-
-            char letter;
-
-            for (int i = 0; i < length; i++)
-            {
-                double flt = random.NextDouble();
-                int shift = Convert.ToInt32(Math.Floor(25 * flt));
-                letter = Convert.ToChar(shift + 65);
-                str_build.Append(letter);
-            }
-            return str_build.ToString();
-            System.Console.WriteLine(str_build.ToString());
-        }
-
+        public static int numprocesses = 4;
+        public static int time = 0;
+        public static int maxtime = 0;
+        public static int quantum = 2;
+        public static int count = 0;
+        public static float turnaround;
+        public static int intermediate = 0;
 
         static void Main(string[] args)
         {
@@ -153,44 +151,58 @@ namespace Oshw3
                 //loop through list of processes in the array
                 for (int i = 0; i < numprocesses; i++)
                 {
-                    //if process is ready to run
+                    //if process at location i is ready to run
                     if (processes[i].activated == true)
                     {
                         while (processes[i].runtime != processes[i].bursttime)
                         {
                             processes[i].runtime++; 
-                            count += 1;
+                            processes[i].tempcount += 1;
                             Console.WriteLine($"Process {processes[i].processname} has run at time slot {time}");
+                            //Console.WriteLine($"Process {processes[i].processname} has been running for {processes[i].tempcount}");
+
+                            for (int c = i + 1; c < numprocesses; c++)
+                            {
+                                int temp = c;
+                                while (processes[i].prio == processes[temp].prio)
+                                {
+                                    processes[c].tempcount = 0;
+                                    temp++;
+                                }
+                            }
+
                             break;
                         }
                         //process has finished
                         if (processes[i].runtime == processes[i].bursttime && processes[i].activated == true)
                         {
-                            Console.WriteLine($"{processes[i].processname} has finished at time {time+1}");
+                            Console.WriteLine($"{processes[i].processname} has left the building at time {time+1}");
                             processes[i].activated = false;
                             processes[i].endtime = time + 1; //sets the time process completed to calculate turn around
                         }
 
-                        if (count == quantum && processes[i].prio == processes[i+1].prio) //time for round robin
+                        if (i + 1 < numprocesses) // there is nothing next in line
                         {
-                            temp = processes[i];
-                            int c = i;
-                            while (processes[c].prio == processes[c + 1].prio)
+                            if (processes[i].tempcount == quantum && processes[i].prio == processes[i + 1].prio) //time for round robin change order of queue
                             {
-                                processes[c] = processes[c + 1];
-                                c++;
+                                temp = processes[i];
+                                int c = i;
+                                while (processes[c].prio == processes[c + 1].prio)
+                                {
+                                    processes[c] = processes[c + 1];
+                                    //temp.tempcount = 0;
+                                    c++;
+                                }
+                                processes[c] = temp;
+                                temp = null;
                             }
-                            processes[c]=temp;
                         }
-
 
                         break;
                     }
                 }
                 time++;
             }
-            float turnaround;
-            int intermediate = 0;
 
             for (int i = 0; i < numprocesses; i++)
             {
@@ -206,5 +218,3 @@ namespace Oshw3
         }
     }
 }
-
-
